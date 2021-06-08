f8a0f6f3e23a9e6d8167cd2e375619cf33d5b108
Ishan Sheth <ishan@coffeebeans.io>
2021-06-08 21:31:54 +0530

    Fixes banner image issue, removes social shares

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,11 @@ module.exports = {
       options: {
         name: `imsheth`,
         short_name: `imsheth`,
+        description: `Ishan Sheth (imsheth) is a seasoned polyglot and can be found in the terminal, the browser, eating, cooking or amongst nature. As a software engineer, he has experienced the rollercoaster journeys of building products from the ground up, onboarding the first customers for products which were eventually acquired and has also offered specialized consultations to corporations which operate at a multi million dollar scale. Owing to this he has got communication, bringing order to chaos and closure under his belt, which are now his forte. Trance music gets him high and keeps him going on with life.`,
         start_url: `/`,
+        lang: `en`,
         background_color: `#231F20`,
+        theme_color: `#231F20`,
         display: `standalone`,
         icon: `static/meta/favicon.png`,
       },
